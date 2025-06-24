b55326a84c2dc3abe1451f75f255e800fcce1d13
Striker797 <kellerjennifer909@gmail.com>
2025-06-24 07:00:43 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-700">Welcome to TRX</h1>
+      <p className="mt-4 text-lg">Invest ₦3,500 and earn ₦900 daily.</p>
+    </div>
+  );
+}
+export default App;
